c152d410cfdb83bf5a8d6788d6d9318fb50f78f0
Santiago <santyvano@outlook.com>
2025-03-19 13:08:23 -0500

    docs: :memo: comments added to make more understantable the program.cs file

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace inheritance_exercise
         static void Main()
         
         {
+           
+            List<Product> products = new List<Product>();
+
             Random random = new Random();
-            //food product names
+            //food product lists
             List<string> foodProductNames = new List<string> { "Bread", "Milk", "Eggs", "Cheese", "Butter" };
             List<int> foodPrices = new List<int> { 2000, 3000, 4000, 5000, 6000 };
             List<string> foodDescriptions = new List<string> {
@@ -17,31 +20,36 @@ namespace inheritance_exercise
                "Eggs are the prefered food of doctor Baiter",
                "Cheese is a dairy product, derived from milk and produced in wide ranges of flavors, textures and forms by coagulation of the milk protein casein.",
                "Butter is a dairy product made from the fat and protein components of milk or cream. It is a semi-solid emulsion at room temperature, consisting of approximately 80% butterfat." };
-
             List<string> foodCategories = new List<string> { "Bakery", "Dairy" };
+
+            //electronic product lists
             List<string> electronicBrands = new List<string> { "Samsung", "Lenovo" };
             List<string> electronicModels = new List<string> { "Galaxy S21", "thinkpad", };
             List<string> electronicProductNames = new List<string> { "Cellphone", "Computer" };
+            List<int> electronicPrices = new List<int> { 200000, 300000 };
             List<string> electronicDescriptions = new List<string> {
                "A cellphone is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area.", "A cellphone is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area.",
                " A computer is a device useful for programming tasks"
            };
-            List<int> electronicPrices = new List<int> { 200000, 300000 };
 
-            List<Product> products = new List<Product>();
+            
+            //generate and save food products using product lists
             for (int i = 0; i < foodProductNames.Count; i++)
             {
-                
+
                 string foodCategory = foodCategories[random.Next(0,foodCategories.Count)];
                 products.Add(new FoodProduct(foodProductNames[i], foodDescriptions[i], foodPrices[i], foodCategory));
             }
 
-            List<ElectronicProduct> electronicProducts = new List<ElectronicProduct>();
+
+            //generate and save electronic products using product lists
             for (int i = 0; i < electronicProductNames.Count; i++)
             {
                 products.Add(new ElectronicProduct(electronicProductNames[i], electronicDescriptions[i], electronicPrices[i], electronicBrands[i], electronicModels[i]));
             }
 
+
+            //display all products with their details
             foreach (var product in products)
             {
                 Console.WriteLine(product.GetDetails());
